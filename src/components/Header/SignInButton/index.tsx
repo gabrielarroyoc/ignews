175274ab1aa3b30641749ff2fc44fd587e1bdca2
gabrielarroyoc <gabrielarroyoc18@gmail.com>
2022-11-09 08:11:58 -0300

    first commit

---
+++ src/components/Header/SignInButton/index.tsx
@@ -0,0 +1,29 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
+import styles from "../SignInButton/styles.module.scss";
+
+export function SignInButton() {
+  const { data: session } = useSession();
+
+  return session ? (
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04d361" />
+      {session.user?.name}
+      <FiX className={styles.closeIcon} color="#737380" />
+    </button>
+  ) : (
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signIn()}
+    >
+      <FaGithub color="#eba417" />
+      SignIn with Github
+    </button>
+  );
+}
---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,27 @@
+import { useSession } from "next-auth/react";
+import styles from "./styles.module.scss";
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+    
+    //criação da checkout session
+  }
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
+}
